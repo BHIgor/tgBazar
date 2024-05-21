6b0a021963c0e055dad191d569a005d37f76455e
Igor <luiz23@ukr.net>
2024-05-21 09:50:52 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     fetch(`https://tgbazar.com.ua/products`, {
       method: 'POST',
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        'Content-type': 'application/json',
      },
    body: JSON.stringify({nameShop: search.substring(1)})
    })
