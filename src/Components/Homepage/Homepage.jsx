f03723a54f4b75fca8a8ffd4d05ad29c922a6f59
Igor <luiz23@ukr.net>
2024-05-21 11:41:02 +0300

    fix

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -5,44 +5,39 @@ const tg = window.Telegram.WebApp;
 
 export const Homepage = () =>{
   const dataDB = useContext(ReactContext);
-  const product = dataDB.products;
-  const user = dataDB.users;
 
   tg.ready()
-  console.log(dataDB)
-  console.log(product)
 
-  console.log(user)
   const onClose = () => {
     tg.close();
   }
   return <> 
-      { (dataDB.length === 0) ? <div>Помилка</div> : <>
+    { (dataDB.length === 0) ? <div>Помилка</div> : <>
+      wordіі
+
+      <button onClick={onClose}>Закрить</button>
+      {tg.initDataUnsafe?.user?.username}
+      {tg.initDataUnsafe?.user?.id}
       
-      word
-    <button onClick={onClose}>Закрить</button>
-    {tg.initDataUnsafe?.user?.username}
-    {tg.initDataUnsafe?.user?.id}
-    
-    {product.map(e => {
-      return (
-        <div key={e.id}>
-          {e.name}
-        </div>
-      )
-    }) 
+      {
+        dataDB.products.map(e => {
+          return (
+            <div key={e.id}>
+              {e.name}
+            </div>
+          )
+        }) 
+      }
+      {
+        dataDB.users.map(e => {
+          return (
+            <div key={e.id}>
+              {e.name}
+            </div>
+          )
+        })
+      }
+      </>
     }
-     { user.map(e => {
-      return (
-        <div key={e.id}>
-          {e.name}
-        </div>
-      )
-    })
-    
-    }
-    </>
-  }
-  
   </>
 }
