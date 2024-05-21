a0b1d84c4c45535201ef39b998884228f0399e22
Igor <luiz23@ukr.net>
2024-05-21 12:31:01 +0300

    sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Routes, Route} from 'react-router-dom';
 
-import './App.css';
+import './App.scss';
 
 import { Homepage } from './Components/Homepage/Homepage';
 import { ReactContext } from './context/ReactContext';
