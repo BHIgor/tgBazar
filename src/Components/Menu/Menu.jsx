7e76f698d3d9a1a420107c605ca1202553499aad
Igor <luiz23@ukr.net>
2024-05-22 14:07:50 +0300

    fix

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -8,45 +8,55 @@ export const Menu = () =>{
 
   return <>
       { (dataDB.length === 0) ? <div>Помилка</div> : <>   
-        <aside className="menu page__menu" id="menu" style={{backgroundColor: '#e9e9e9'}}>
-            <div className='menu__title' style={{color:'orange'}}>
+        <aside className="menu page__menu" id="menu" >
+            <div className='menu__title' style={{color:'orange',backgroundColor: 'black'}}>
               {dataDB.listBot[0].name}
             </div>
             <div className="container menu__container">
-            <ul className="menu__nav">
-              <li className="menu__item">
-              </li>
+              <ul className="menu__nav">
+                <li className="menu__item">
+                  <a href="#About" className="menu__list">
+                    Головна
+                  </a>
+                </li>
 
-              <li className="menu__item">
-                <a href="#About" className="menu__list">
-                  About Us
-                </a>
-              </li>
+                <li className="menu__item">
+                  <a href="#Compare" className="menu__list">
+                    Каталог
+                  </a>
+                </li>
 
-              <li className="menu__item">
-                <a href="#Compare" className="menu__list">
-                  Compare Bikes
-                </a>
-              </li>
+                <li className="menu__item">
+                  <a href="#Details" className="menu__list">
+                    Кошик
+                  </a>
+                </li>
 
-              <li className="menu__item">
-                <a href="#Details" className="menu__list">
-                  Details
-                </a>
-              </li>
-
-              <li className="menu__item">
-                <a href="#Contact" className="menu__list">
-                  Contacts
-                </a>
-              </li>
-            </ul>
+                <li className="menu__item">
+                  <a href="#Contact" className="menu__list">
+                    Мої замовлення
+                  </a>
+                </li>
+              </ul>
             <div className="menu__footer">
               <a href="tel:+1 234 5555-55-55" className="menu__list--link">
-                +1 234 5555-55-55
+                Про нас
               </a>
-
-
+              <a href="tel:+1 234 5555-55-55" className="menu__list--link">
+                Контакти
+              </a>
+              <a href="tel:+1 234 5555-55-55" className="menu__list--link">
+                Доставка
+              </a>
+              <a href="tel:+1 234 5555-55-55" className="menu__list--link">
+                Оплата
+              </a>
+              <a href="tel:+1 234 5555-55-55" className="menu__list--link">
+                Обмін і повернення
+              </a>
+              <a href="tel:+1 234 5555-55-55" className="menu__list--link">
+                Гарантійні умови
+              </a>
             </div>
           </div>
       
