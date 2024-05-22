bae17631264686a915e208872155555bbe9020c7
Igor <luiz23@ukr.net>
2024-05-22 10:18:19 +0300

    fix

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -5,43 +5,43 @@ export const Menu = () =>{
   return <>   
     <aside className="menu page__menu" id="menu">
       <div className="container menu__container">
-        <ul class="menu__nav">
-          <li class="menu__item">
-            <a href="#" class="menu__list">
+        <ul className="menu__nav">
+          <li className="menu__item">
+            <a href="#" className="menu__list">
               Home
             </a>
           </li>
 
-          <li class="menu__item">
-            <a href="#About" class="menu__list">
+          <li className="menu__item">
+            <a href="#About" className="menu__list">
               About Us
             </a>
           </li>
 
-          <li class="menu__item">
-            <a href="#Compare" class="menu__list">
+          <li className="menu__item">
+            <a href="#Compare" className="menu__list">
               Compare Bikes
             </a>
           </li>
 
-          <li class="menu__item">
-            <a href="#Details" class="menu__list">
+          <li className="menu__item">
+            <a href="#Details" className="menu__list">
               Details
             </a>
           </li>
 
-          <li class="menu__item">
-            <a href="#Contact" class="menu__list">
+          <li className="menu__item">
+            <a href="#Contact" className="menu__list">
               Contacts
             </a>
           </li>
         </ul>
-        <div class="menu__footer">
-          <a href="tel:+1 234 5555-55-55" class="menu__list--link">
+        <div className="menu__footer">
+          <a href="tel:+1 234 5555-55-55" className="menu__list--link">
             +1 234 5555-55-55
           </a>
 
-          <a href="#" class="menu__list--link">
+          <a href="#" className="menu__list--link">
             Book a test ride
           </a>
         </div>
