59f98af8734f06d9f134a6c5ddb62e057e9e1db9
Igor <luiz23@ukr.net>
2024-05-23 18:08:20 +0300

    fix

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,11 +1,14 @@
 import { useContext } from 'react';
 import { ReactContext } from "../../context/ReactContext"
+import { NavLink } from 'react-router-dom';
 
 import '../Menu/Menu.scss';
 
 export const Menu = () =>{
   const dataDB = useContext(ReactContext);
 
+  const getLinkStyle = ({ isActive }) => ({backgroundColor: isActive ? `${dataDB.settings[0].clHeader}`: null}) 
+  
   return <>
       { (dataDB.length === 0) ? <div>Помилка</div> : <>   
         <aside className="menu page__menu" id="menu" >
@@ -15,51 +18,66 @@ export const Menu = () =>{
           </div>
           <div className="container menu__container">
               <ul className="menu__nav">
-                <li className="menu__item" style={{backgroundColor:`${dataDB.settings[0].clHeader}`}}>
-                  <div className='menu__icon menu__icon--main'></div>
-                  <a href="#About" className="menu__list">
-                    Головна
-                  </a>
-                </li>
-                <li className="menu__item">
-                  <div className='menu__icon menu__icon--katalog'></div>
-                  <a href="#Compare" className="menu__list">
-                    Каталог
-                  </a>
-                </li>
-                <hr className='menu__lineHorizont'></hr>
-                <li className="menu__item">
-                  <div className='menu__icon menu__icon--cart'></div>
-                  <a href="#Details" className="menu__list">
-                    Кошик
-                  </a>
-                </li>
+                <NavLink to="/" className='menu__link' style={getLinkStyle}>
+                  <li className="menu__item">
+                    <div className='menu__icon menu__icon--main'></div>
+                      <div className='menu__list' >
+                        Головна
+                      </div>
+                  </li>
+                </NavLink>
 
-                <li className="menu__item ">
-                  <div className='menu__icon menu__icon--checkout'></div>
-                  <a href="#Contact" className="menu__list">
-                    Мої замовлення
-                  </a>
-                </li>
+                <NavLink to="/Compare" className='menu__link' style={getLinkStyle}>
+                  <li className="menu__item">
+                    <div className='menu__icon menu__icon--katalog'></div>
+                    <div className="menu__list">
+                      Каталог
+                    </div>
+                  </li>
+                </NavLink>
+
+                <hr className='menu__lineHorizont'></hr>
+
+                <NavLink to="/Cart" className='menu__link' style={getLinkStyle}>
+                  <li className="menu__item">
+                    <div className='menu__icon menu__icon--cart'></div>
+                    <div className="menu__list">
+                      Кошик
+                    </div>
+                  </li>
+                </NavLink>
+
+                <NavLink to="/Orders" className='menu__link' style={getLinkStyle}>
+                  <li className="menu__item ">
+                    <div className='menu__icon menu__icon--checkout'></div>
+                    <div className="menu__list">
+                      Мої замовлення
+                    </div>
+                  </li>
+                </NavLink>
 
                 { 
                   (dataDB.settings[0].contacts === '') ? null : (
-                    <li className="menu__item">
-                      <div className='menu__icon menu__icon--contacts'></div>
-                      <a href="#Contact" className="menu__list">
-                        Контакти
-                      </a>
-                    </li>
+                    <NavLink to="/Contact" className='menu__link' style={getLinkStyle}>
+                      <li className="menu__item">
+                        <div className='menu__icon menu__icon--contacts'></div>
+                        <div className="menu__list">
+                          Контакти
+                        </div>
+                      </li>
+                    </NavLink>
                   )
                 }
                 { 
                   (dataDB.settings[0].help === '') ? null : (
-                    <li className="menu__item">
-                      <div className='menu__icon menu__icon--help'></div>
-                      <a href="#Contact" className="menu__list">
-                        Допомога
-                      </a>
-                    </li>
+                    <NavLink to="/Help" className='menu__link' style={getLinkStyle}>
+                      <li className="menu__item">
+                        <div className='menu__icon menu__icon--help'></div>
+                        <div className="menu__list">
+                          Допомога
+                        </div>
+                      </li>
+                    </NavLink>
                   )
                 }
 
@@ -67,67 +85,79 @@ export const Menu = () =>{
                 
                 { 
                   (dataDB.settings[0].about === '') ? null : (
-                    <li className="menu__item">
-                      <div className='menu__icon menu__icon--about'></div>
-                      <a href="#Contact" className="menu__list">
-                        Про нас
-                      </a>
-                    </li>
+                    <NavLink to="/About" className='menu__link' style={getLinkStyle}>
+                      <li className="menu__item">
+                        <div className='menu__icon menu__icon--about'></div>
+                        <div className="menu__list">
+                          Про нас
+                        </div>
+                      </li>
+                    </NavLink>
                   )
                 }
                 
                 { 
                   (dataDB.settings[0].garant === '') ? null : (
-                    <li className="menu__item">
-                      <div className='menu__icon menu__icon--garant'></div>
-                      <a href="#Contact" className="menu__list">
-                        Гарантійні умови
-                      </a>
-                    </li>
+                    <NavLink to="/Garant" className='menu__link' style={getLinkStyle}>
+                      <li className="menu__item">
+                        <div className='menu__icon menu__icon--garant'></div>
+                        <div className="menu__list">
+                          Гарантійні умови
+                        </div>
+                      </li>
+                    </NavLink>
                   )
                 }
 
                 { 
                   (dataDB.settings[0].delivery === '') ? null : (
-                    <li className="menu__item">
-                      <div className='menu__icon menu__icon--delivery'></div>
-                      <a href="#Contact" className="menu__list">
-                        Доставка
-                      </a>
-                    </li>
+                    <NavLink to="/Delivery" className='menu__link' style={getLinkStyle}>
+                      <li className="menu__item">
+                        <div className='menu__icon menu__icon--delivery'></div>
+                        <div  className="menu__list">
+                          Доставка
+                        </div>
+                      </li>
+                    </NavLink>
                   )
                 }
                
                 { 
                   (dataDB.settings[0].pay === '') ? null : (
-                    <li className="menu__item">
-                      <div className='menu__icon menu__icon--pay'></div>
-                      <a href="#Contact" className="menu__list">
-                        Оплата
-                      </a>
-                    </li>
+                    <NavLink to="/Pay" className='menu__link' style={getLinkStyle}>
+                      <li className="menu__item">
+                        <div className='menu__icon menu__icon--pay'></div>
+                        <div className="menu__list">
+                          Оплата
+                        </div>
+                      </li>
+                    </NavLink>
                   )
                 }
                 
                 { 
                   (dataDB.settings[0].obmin === '') ? null : (
-                    <li className="menu__item ">
-                      <div className='menu__icon menu__icon--obmin'></div>
-                      <a href="#Contact" className="menu__list">
-                        Обмін і повернення
-                      </a>
-                    </li>
+                    <NavLink to="/Obmin" className='menu__link' style={getLinkStyle}>
+                      <li className="menu__item ">
+                        <div className='menu__icon menu__icon--obmin'></div>
+                        <div className="menu__list">
+                          Обмін і повернення
+                        </div>
+                      </li>
+                    </NavLink>
                   )
                 }
 
                 { 
                   (dataDB.settings[0].grafik === '') ? null : (
-                    <li className="menu__item">
-                      <div className='menu__icon menu__icon--time'></div>
-                      <a href="#Contact" className="menu__list">
-                        Графік роботи
-                      </a>
-                    </li>
+                    <NavLink to="/Grafik" className='menu__link' style={getLinkStyle}>
+                      <li className="menu__item">
+                        <div className='menu__icon menu__icon--time'></div>
+                        <div className="menu__list">
+                          Графік роботи
+                        </div>
+                      </li>
+                    </NavLink>
                   )
                 }
               </ul>
