a82f29e4eec9006018ea2349dcb6aadc0876fc9d
Igor <luiz23@ukr.net>
2024-05-24 10:56:41 +0300

    fix

--- src/Components/Homepage/Slider/Slider.jsx
+++ src/Components/Homepage/Slider/Slider.jsx
@@ -11,7 +11,7 @@ import './Slider.scss';
 // import required modules
 import { Autoplay, Pagination } from 'swiper/modules';
 
-const imgas = ['https://i.work.ua/article/579b.jpg', 'https://www.interfax.ru/ftproot/photos/photostory/2019/07/09/week4_700.jpg', 'https://lifehacker.ru/special/fujifilm/dist/static/img/5.2410a2d.jpg', 'https://tengrinews.kz/userdata/news/2021/news_454230/thumb_m/photo_380266.jpeg']
+const imgas = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJFp_lAOAtI0MtZEK9tjkVbs24r35f0Mx96wkDsMGO0ETZdmfNaZLC4hfEAU_YD_oydcQ&usqp=CAU', 'https://www.interfax.ru/ftproot/photos/photostory/2019/07/09/week4_700.jpg', 'https://lifehacker.ru/special/fujifilm/dist/static/img/5.2410a2d.jpg', 'https://tengrinews.kz/userdata/news/2021/news_454230/thumb_m/photo_380266.jpeg']
 
 export const Slider = () => {
   return (
@@ -29,7 +29,7 @@ export const Slider = () => {
         {
           imgas.map(e => {
             return (
-              <SwiperSlide><img src={e} /></SwiperSlide>
+              <SwiperSlide key={e}><img src={e} alt='img-slider'/></SwiperSlide>
             )
           })
         }
