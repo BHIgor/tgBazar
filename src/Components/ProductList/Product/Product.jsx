09b14bebb393cfde63e7dd8971475e468f698344
Igor <luiz23@ukr.net>
2024-05-25 17:04:41 +0300

    fix

--- src/Components/ProductList/Product/Product.jsx
+++ src/Components/ProductList/Product/Product.jsx
@@ -12,22 +12,22 @@ export const Product = ({products}) =>{
   return <> 
     { (dataDB.length === 0) ? <div>Помилка</div> : <>
       <div className="product">
-        <div className='s'>
-        <div className='product__title'>Топ продажу</div>
-        <div className="product__container">
-          {products.map(e => {
-            return (
-              <div key={e.id} className='product__page'>
-                <img 
-                  src={e.image} 
-                  alt='photo_product' 
-                  className='product__image'
-                  />
+        <div className='product__block'>
+          <div className='product__title'>Топ продажу</div>
+          <div className="product__container">
+            {products.map(e => {
+              return (
+                <div key={e.id} className='product__page'>
+                  <img 
+                    src={e.image} 
+                    alt='photo_product' 
+                    className='product__image'
+                    />
 
-              </div>
-            )
-          })}
-        </div> 
+                </div>
+              )
+            })}
+          </div> 
         </div> 
       </div>
     </>
