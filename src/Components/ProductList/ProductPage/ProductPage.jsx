45a31f456bdda6a4bb4ab6fe04c79272beda76b7
Igor <luiz23@ukr.net>
2024-05-30 10:50:51 +0300

    fsa

--- src/Components/ProductList/ProductPage/ProductPage.jsx
+++ src/Components/ProductList/ProductPage/ProductPage.jsx
@@ -110,7 +110,7 @@ export const ProductPage = () =>{
                 </div>
                 { // onClick={() => (copy) ? setCopy(false) : setCopy(true)}
                   (dataDB.listBot[0].linkShop !== '') ? 
-                  <a href="tg://msg?text='saf'"  className={`productPage__share ${copy ? 'productPage__shareActive' : null}`}>
+                  <a href="tg:share?text='saf'"  className={`productPage__share ${copy ? 'productPage__shareActive' : null}`}>
                     <div className={`productPage__share--${copy ? 'iconActive' :'icon'} `}></div>
                   </a> 
                 :null
