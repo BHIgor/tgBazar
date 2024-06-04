0312495f4526c45df065616095ba2c7b64467092
Igor <luiz23@ukr.net>
2024-06-04 14:09:34 +0300

    fix

--- src/Components/Menu/Cart/Checkout/Checkout.jsx
+++ src/Components/Menu/Cart/Checkout/Checkout.jsx
@@ -5,7 +5,13 @@ import './Checkout.scss'
 
 export const Checkout = () =>{
   const { dataDB } = useContext(ReactContext);
-  const [ status ] = useState('dostavka')
+  const [ status, setStatus ] = useState('contact')
+  const [ user, setUser ] = useState({fio: '', phone: ''})
+
+  
+  const fullPrice = (dataDB.cart) ? dataDB.cart.reduce((accumulator, currentValue) => {
+   return accumulator + (((currentValue.price_discount === 0)? currentValue.price : currentValue.price_discount) * currentValue.count);
+  }, 0) : 0
 
   return <> 
     { (dataDB.length === 0) ? <div>Помилка</div> : <>
@@ -20,7 +26,81 @@ export const Checkout = () =>{
             <div className={(status === 'dostavka') ? "checkout__icons--dostavkaActiv" : "checkout__icons--dostavka"}></div>
             <hr className="checkout__icons--line"></hr>
             <div className={(status === 'oplata') ? "checkout__icons--oplataActiv" : "checkout__icons--oplata"}></div>  
-          </div>       
+          </div> 
+          <div className="checkout__body">
+            {(status === 'contact') ? <>
+              <div className="checkout__contact">
+                <div className="checkout__contact--title">
+                  Ваші дані
+                </div>
+                <input 
+                  className="checkout__contact--input" 
+                  placeholder={`Прізвище, ім'я, по батькові *`}
+                  onChange={(event) => setUser(prevstate => ({
+                    ...prevstate,
+                    fio: event.target.value
+                  }))}
+                  required  
+                />
+                <input 
+                  className="checkout__contact--input" 
+                  placeholder={`Ваш номер телефону *`}
+                  onChange={(event) => setUser(prevstate => ({
+                    ...prevstate,
+                    phone: event.target.value
+                  }))}
+                  type="number" 
+                  required  
+                />
+              
+                <button  
+                  className="checkout__contact--next" 
+                  style={{backgroundColor: `${dataDB.settings[0].clHeader}`}}
+                  onClick={() => setStatus('dostavka')}
+                  disabled={(user.fio === '' || user.phone === '')? true:false}
+                  >
+                  Далі
+                </button >
+              </div>
+            </>:null} 
+
+            {(status === 'dostavka') ? <>
+              <div className="checkout__dostavka">
+                <div className="checkout__dostavka--title">
+                  Доставка
+                </div>
+              
+              </div>
+            </>:null} 
+
+            {(status === 'oplata') ? <>
+              <div className="checkout__oplata">
+                <div className="checkout__oplata--title">
+                  Оплата
+                </div>
+              
+              </div>
+            </>:null} 
+          </div>
+           <div className="cart__orderBlock">
+            <div className="cart__order"  style={{backgroundColor: `${dataDB.settings[0].clHeader}`}}></div> 
+  
+            <div className="cart__fullPrice">
+              <div className="cart__blockPrice">
+                <div className="cart__fullPrice--text">
+                  Разом:
+                </div>
+                <div className="cart__fullPrice--price">
+                  {fullPrice}<span className='cart__simvol'>₴</span>
+                </div>
+              </div>
+  
+              <div to={`/Checkout?${dataDB.listBot[0].nameShop}`}  className="cart__orderButton checkout__enter" style={{backgroundColor: `${dataDB.settings[0].clButtonProduct}`}}>
+                Замовлення підтверджую
+              </div>
+            </div>
+  
+           </div>      
         </div>
       </div>
     </>
