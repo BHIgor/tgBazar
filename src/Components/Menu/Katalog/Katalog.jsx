dd4acc0dcff74b4513babe9865c4640ba11cb1c5
Igor <luiz23@ukr.net>
2024-06-17 13:55:59 +0300

    fix

--- src/Components/Menu/Katalog/Katalog.jsx
+++ src/Components/Menu/Katalog/Katalog.jsx
@@ -5,46 +5,52 @@ import './Katalog.scss'
 import { Link } from 'react-router-dom';
 import { Search } from '../../Homepage/Search/Search';
 
-export const Katalog = () =>{
+export const Katalog = () => {
   const { dataDB } = useContext(ReactContext);
 
-  const allProducts = (dataDB.length === 0)? [] :dataDB.products
+  const allProducts = (dataDB.length === 0) ? [] : dataDB.products
 
   const uniqueCategories = new Set(allProducts.map(product => product.kategory));
   const uniquArray = Array.from(uniqueCategories);
 
   const arrTag = [];
 
-  for(let i = 0; i < uniquArray.length; i = i + 2){
-      arrTag.push(<div className='katalog__block' key={i} >   
+  for (let i = 0; i < uniquArray.length; i = i + 2) {
+    arrTag.push(<div className='katalog__block' key={i} >
       {
         (uniquArray[i + 1] !== undefined) ?
           <div className="katalog__container">
-            <Link  to={`/Kategory/${uniquArray[i]}?${dataDB.listBot[0].nameShop}`} className="katalog__item">
-              {uniquArray[i]}  
+            <Link to={`/Kategory/${uniquArray[i]}?${dataDB.listBot[0].nameShop}`} className="katalog__item">
+              {uniquArray[i]}
             </Link>
 
-            <Link to={`/Kategory/${uniquArray[i+1]}?${dataDB.listBot[0].nameShop}`}  className="katalog__item">
-              {uniquArray[i + 1]}  
-            </Link> 
-          </div>  
-        : 
-        <div className="katalog__container" style={{gridTemplateColumns: '1fr'}}>
-          <Link to={`/Kategory/${uniquArray[i]}?${dataDB.listBot[0].nameShop}`}  className="katalog__item">
-            {uniquArray[i]}  
-          </Link>
-        </div>  
+            <Link to={`/Kategory/${uniquArray[i + 1]}?${dataDB.listBot[0].nameShop}`} className="katalog__item">
+              {uniquArray[i + 1]}
+            </Link>
+          </div>
+          :
+          <div className="katalog__container" style={{ gridTemplateColumns: '1fr' }}>
+            <Link to={`/Kategory/${uniquArray[i]}?${dataDB.listBot[0].nameShop}`} className="katalog__item">
+              {uniquArray[i]}
+            </Link>
+          </div>
       }
-      
+
     </div>)
   }
 
-  return <> 
-    { (dataDB.length === 0) ? <div>Помилка</div> : <>
-      <Search/>
+  return <>
+    {(dataDB.length === 0) ? <div>Помилка</div> : <>
+      <Search />
       <div className="titleText">Каталог</div>
+
       <div className='katalog'>
-          {arrTag}
+        <div className="katalog__allproduct">
+          <Link to={`/Kategory/Всі товари?${dataDB.listBot[0].nameShop}`} className="katalog__item katalog__item2">
+            Всі товари
+          </Link>
+        </div>
+        {arrTag}
       </div>
     </>
     }
--- src/Components/Menu/Katalog/Kategory.jsx
+++ src/Components/Menu/Katalog/Kategory.jsx
@@ -1,4 +1,4 @@
-import React,{ useContext, useEffect, useMemo, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { ReactContext } from "../../../context/ReactContext"
 import { Link, useParams } from 'react-router-dom';
 import { Product } from '../../ProductList/Product/Product';
@@ -11,30 +11,30 @@ import 'swiper/css/pagination';
 
 import { Autoplay } from 'swiper/modules';
 
-export const Kategory = () =>{
+export const Kategory = () => {
   const { dataDB } = useContext(ReactContext);
   const { catageryName } = useParams();
   const [sortedItems, setSortedItems] = useState([]);
   const [sortType, setSortType] = useState('');
 
-  const allProducts =  useMemo(() =>(dataDB.length ===0 ) ? []: dataDB.products, [dataDB.length, dataDB.products])
+  const allProducts = useMemo(() => (dataDB.length === 0) ? [] : dataDB.products, [dataDB.length, dataDB.products])
 
 
   const scrollToTop = () => {
-    scroll.scrollToTop({duration:20});
+    scroll.scrollToTop({ duration: 20 });
   };
 
   useEffect(() => {
-    const filtered = allProducts.filter(e => e.kategory === catageryName);
+    const filtered = (catageryName === 'Всі товари') ? allProducts : allProducts.filter(e => e.kategory === catageryName);
     setSortedItems(filtered);
 
-  },[dataDB.products, catageryName, allProducts]);
+  }, [dataDB.products, catageryName, allProducts]);
 
   const uniqueCategories = new Set(allProducts.map(product => product.kategory));
   const uniquArray = Array.from(uniqueCategories);
-  
+
   const sortByPrice = (order) => {
-    const sorted = sortedItems.slice().sort((a, b) => (order === 'desc' ? ((b.price_discount === 0) ? b.price: b.price_discount) - ((a.price_discount === 0) ? a.price: a.price_discount) : ((a.price_discount === 0) ? a.price: a.price_discount) - ((b.price_discount === 0) ? b.price: b.price_discount)));
+    const sorted = sortedItems.slice().sort((a, b) => (order === 'desc' ? ((b.price_discount === 0) ? b.price : b.price_discount) - ((a.price_discount === 0) ? a.price : a.price_discount) : ((a.price_discount === 0) ? a.price : a.price_discount) - ((b.price_discount === 0) ? b.price : b.price_discount)));
     setSortType(order);
     setSortedItems(sorted);
     console.log(sorted)
@@ -57,8 +57,8 @@ export const Kategory = () =>{
     }
   };
 
-  return <> 
-    { (dataDB.length === 0) ? <div>Помилка</div> : <>
+  return <>
+    {(dataDB.length === 0) ? <div>Помилка</div> : <>
       <div className="kategory">
         <div className="kategory__container">
 
@@ -71,20 +71,28 @@ export const Kategory = () =>{
           <Swiper
             slidesPerView={3}
             spaceBetween={10}
-            modules={[Autoplay]} 
+            modules={[Autoplay]}
             autoplay={{
               delay: 3000,
               disableOnInteraction: false,
             }}
             className="kategory__slider"
-          > 
+          >
+            <SwiperSlide className='kategory__slider--item' >
+              <Link
+                to={`/Kategory/Всі товари?${dataDB.listBot[0].nameShop}`} className='kategory__slider--link'
+                style={{ border: `1px solid ${dataDB.settings[0].clHeader}` }}
+              >
+                Всі товари
+              </Link>
+            </SwiperSlide>
             {
-              uniquArray.map(e =>{
+              uniquArray.map(e => {
                 return (
                   <SwiperSlide className='kategory__slider--item' key={e}  >
-                    <Link 
+                    <Link
                       to={`/Kategory/${e}?${dataDB.listBot[0].nameShop}`} className='kategory__slider--link'
-                      style={{border: `1px solid ${dataDB.settings[0].clHeader}`}}
+                      style={{ border: `1px solid ${dataDB.settings[0].clHeader}` }}
                     >
                       {e}
                     </Link>
@@ -95,13 +103,13 @@ export const Kategory = () =>{
           </Swiper>
 
           <div className="kategory__sort">
-            <Link to={`/Katalog?${dataDB.listBot[0].nameShop}`}    onClick={() =>scrollToTop()}  className="kategory__back" style={{backgroundColor: `${dataDB.settings[0].clButtonProduct}`}}>
+            <Link to={`/Katalog?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className="kategory__back" style={{ backgroundColor: `${dataDB.settings[0].clButtonProduct}` }}>
               Назад
             </Link>
-           <select 
-            defaultValue={sortType} 
-            onChange={handleSortChange} 
-            className="kategory__sort--selected"
+            <select
+              defaultValue={sortType}
+              onChange={handleSortChange}
+              className="kategory__sort--selected"
             >
               <option value="" disabled>Сортувати</option>
               <option value="priceAsc">За зростанням ціни</option>
@@ -110,13 +118,13 @@ export const Kategory = () =>{
               <option value="ratingDesc">За спаданням рейтинга</option>
             </select>
           </div>
-          
+
           <div>
-            <Product products = {sortedItems}/>
+            <Product products={sortedItems} />
           </div>
-        </div>      
+        </div>
       </div>
-     
+
     </>
     }
   </>
