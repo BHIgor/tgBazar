0ae4b2030283607f92b9669c7fefcd65a34f0fa9
Igor <luiz23@ukr.net>
2024-06-20 13:43:45 +0300

    fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,55 +3,56 @@ import { ReactContext } from "../../context/ReactContext"
 
 import './Footer.scss';
 
-export const Footer = ({ setMenu }) =>{
+export const Footer = ({ setMenu }) => {
   const { dataDB } = useContext(ReactContext);
 
-  return <> 
-    { (dataDB.length === 0) ? <div>Помилка</div> : <>
+  return <>
+    {(dataDB.length === 0) ? <div>Помилка</div> : <>
       <footer className="footer">
-        <div className="container footer--container"  style={{backgroundColor: `${dataDB.settings[0].clFooter}`}}>
-          {
-            (dataDB.settings[0].viber !== '' || dataDB.settings[0].telegram !== '' || dataDB.settings[0].instagram !== '' || dataDB.settings[0].facebook !== '' || dataDB.settings[0].tiktok !== '')? 
-            <>
-              <div className='footer__title' style={{color: `${dataDB.settings[0].clFooterTitle}`}}>Ми в соц. мережах:</div>
+        {
+          (dataDB.settings[0].viber !== '' || dataDB.settings[0].telegram !== '' || dataDB.settings[0].instagram !== '' || dataDB.settings[0].facebook !== '' || dataDB.settings[0].tiktok !== '') ?
+
+            <div className="container footer--container" style={{ backgroundColor: `${dataDB.settings[0].clFooter}` }}>
+              <div className='footer__title' style={{ color: `${dataDB.settings[0].clFooterTitle}` }}>Ми в соц. мережах:</div>
               <div className='footer__icons'>
                 {
-                  (dataDB.settings[0].viber !== '') ? <a 
-                    href={dataDB.settings[0].viber} 
-                    target="_blank" 
-                    rel="noopener noreferrer"  className="footer__icons--viber footer__icons--size"><span></span></a>:null
+                  (dataDB.settings[0].viber !== '') ? <a
+                    href={dataDB.settings[0].viber}
+                    target="_blank"
+                    rel="noopener noreferrer" className="footer__icons--viber footer__icons--size"><span></span></a> : null
                 }
                 {
-                  (dataDB.settings[0].telegram !== '') ? <a 
-                    href={dataDB.settings[0].telegram} 
-                    target="_blank" 
-                    rel="noopener noreferrer" className="footer__icons--telegram footer__icons--size"><span></span></a>:null
+                  (dataDB.settings[0].telegram !== '') ? <a
+                    href={dataDB.settings[0].telegram}
+                    target="_blank"
+                    rel="noopener noreferrer" className="footer__icons--telegram footer__icons--size"><span></span></a> : null
                 }
                 {
-                  (dataDB.settings[0].instagram !== '') ? <a 
-                    href={dataDB.settings[0].instagram} 
-                    target="_blank" 
-                    rel="noopener noreferrer"  
-                    className="footer__icons--instagram footer__icons--size"><span></span></a>:null
+                  (dataDB.settings[0].instagram !== '') ? <a
+                    href={dataDB.settings[0].instagram}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="footer__icons--instagram footer__icons--size"><span></span></a> : null
                 }
                 {
-                  (dataDB.settings[0].facebook !== '') ? <a 
-                    href={dataDB.settings[0].facebook} 
-                    target="_blank" 
-                    rel="noopener noreferrer"  
-                    className="footer__icons--facebook footer__icons--size"><span></span></a>:null
+                  (dataDB.settings[0].facebook !== '') ? <a
+                    href={dataDB.settings[0].facebook}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="footer__icons--facebook footer__icons--size"><span></span></a> : null
                 }
                 {
-                  (dataDB.settings[0].tiktok !== '') ? <a 
-                    href={dataDB.settings[0].tiktok} 
-                    target="_blank" 
-                    rel="noopener noreferrer"  
-                    className="footer__icons--tiktok footer__icons--size"><span></span></a>:null
+                  (dataDB.settings[0].tiktok !== '') ? <a
+                    href={dataDB.settings[0].tiktok}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="footer__icons--tiktok footer__icons--size"><span></span></a> : null
                 }
               </div>
-            </> :null
-          }
-        </div>
+            </div>
+            : null
+        }
+
         <a href='https://t.me/tgBazarShop_bot' className='footer__ads'>
           Розроблено за допомогою @tgBazarShop_bot
         </a>
--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -5,187 +5,187 @@ import { animateScroll as scroll } from 'react-scroll';
 
 import '../Menu/Menu.scss';
 
-export const Menu = ({ 
+export const Menu = ({
   menu,
   setMenu
-}) =>{
+}) => {
   const { dataDB } = useContext(ReactContext);
 
-  const getLinkStyle = ({ isActive }) => ({backgroundColor: isActive ? `${dataDB.settings[0].clHeader}`: null}) 
+  const getLinkStyle = ({ isActive }) => ({ backgroundColor: isActive ? `${dataDB.settings[0].clHeader}` : null })
   const scrollToTop = () => {
     setMenu(false)
-    scroll.scrollToTop({duration:20});
+    scroll.scrollToTop({ duration: 20 });
   };
-  
+
   return <>
-      { (dataDB.length === 0) ? <div>Помилка</div> : <>   
-        <aside className={menu ? "menu page__menu active-menu" : 'menu page__menu'}>
-          <div onClick={() => setMenu(false)}  className='menu__close'> </div>
-          <div className='menu__title' style={{color:`${dataDB.settings[0].clHeader}`,backgroundColor: `${dataDB.settings[0].clTitle}`}}>
-            {dataDB.listBot[0].name}
-          </div>
-          <div className="container menu__container">
-              <ul className="menu__nav">
-                <NavLink 
-                  to={`/?${dataDB.listBot[0].nameShop}#back`} 
-                  onClick={() => scrollToTop()}  
-                  className='menu__link' 
-                  style={getLinkStyle}
-               
-                >
+    {(dataDB.length === 0) ? <div>Помилка</div> : <>
+      <aside className={menu ? "menu page__menu active-menu" : 'menu page__menu'}>
+        <div onClick={() => setMenu(false)} className='menu__close'> </div>
+        <div className='menu__title' style={{ color: `${dataDB.settings[0].clHeader}`, backgroundColor: `${dataDB.settings[0].clTitle}` }}>
+          {dataDB.listBot[0].name}
+        </div>
+        <div className="container menu__container">
+          <ul className="menu__nav">
+            <NavLink
+              to={`/?${dataDB.listBot[0].nameShop}#back`}
+              onClick={() => scrollToTop()}
+              className='menu__link'
+              style={getLinkStyle}
+
+            >
+              <li className="menu__item">
+                <div className='menu__icon menu__icon--main'></div>
+                <div className='menu__list' >
+                  Головна
+                </div>
+              </li>
+            </NavLink>
+
+            <NavLink to={`/Katalog?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
+              <li className="menu__item">
+                <div className='menu__icon menu__icon--katalog'></div>
+                <div className="menu__list">
+                  Каталог
+                </div>
+              </li>
+            </NavLink>
+
+            <hr className='menu__lineHorizont'></hr>
+
+            <NavLink to={`/Cart?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
+              <li className="menu__item">
+                <div className='menu__icon menu__icon--cart'></div>
+                <div className="menu__list">
+                  Кошик
+                </div>
+              </li>
+            </NavLink>
+
+            <NavLink to={`/Like?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
+              <li className="menu__item">
+                <div className='menu__icon menu__icon--like'></div>
+                <div className="menu__list">
+                  Обрані
+                </div>
+              </li>
+            </NavLink>
+
+            <NavLink to={`/Orders?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
+              <li className="menu__item ">
+                <div className='menu__icon menu__icon--checkout'></div>
+                <div className="menu__list">
+                  Мої замовлення
+                </div>
+              </li>
+            </NavLink>
+
+            {
+              (dataDB.settings[0].viberContact === '' && dataDB.settings[0].phoneOne === '' && dataDB.settings[0].instaContact === '' && dataDB.settings[0].phoneTwo === '' && dataDB.settings[0].email === '' && dataDB.settings[0].telegaContact === '') ? null : (
+                <NavLink to={`/Contacts?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
                   <li className="menu__item">
-                    <div className='menu__icon menu__icon--main'></div>
-                      <div className='menu__list' >
-                        Головна
-                      </div>
-                  </li>
-                </NavLink>
-                
-                <NavLink to={`/Katalog?${dataDB.listBot[0].nameShop}`} onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
-                  <li className="menu__item">
-                    <div className='menu__icon menu__icon--katalog'></div>
+                    <div className='menu__icon menu__icon--contacts'></div>
                     <div className="menu__list">
-                      Каталог
+                      Контакти
                     </div>
                   </li>
                 </NavLink>
+              )
+            }
 
-                <hr className='menu__lineHorizont'></hr>
+            <NavLink to={`/Help?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
+              <li className="menu__item">
+                <div className='menu__icon menu__icon--help'></div>
+                <div className="menu__list">
+                  Допомога
+                </div>
+              </li>
+            </NavLink>
 
-                <NavLink  to={`/Cart?${dataDB.listBot[0].nameShop}`} onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
+
+            <hr className='menu__lineHorizont'></hr>
+
+            {
+              (dataDB.settings[0].about === '') ? null : (
+                <NavLink to={`/About?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
                   <li className="menu__item">
-                    <div className='menu__icon menu__icon--cart'></div>
+                    <div className='menu__icon menu__icon--about'></div>
                     <div className="menu__list">
-                      Кошик
+                      Про нас
                     </div>
                   </li>
                 </NavLink>
+              )
+            }
 
-                <NavLink  to={`/Like?${dataDB.listBot[0].nameShop}`} onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
+            {
+              (dataDB.settings[0].garant === '') ? null : (
+                <NavLink to={`/Garant?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
                   <li className="menu__item">
-                    <div className='menu__icon menu__icon--like'></div>
+                    <div className='menu__icon menu__icon--garant'></div>
                     <div className="menu__list">
-                      Обрані
+                      Гарантійні умови
                     </div>
                   </li>
                 </NavLink>
+              )
+            }
 
-                <NavLink to={`/Orders?${dataDB.listBot[0].nameShop}`} onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
+            {
+              (dataDB.settings[0].delivery === '') ? null : (
+                <NavLink to={`/Delivery?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
+                  <li className="menu__item">
+                    <div className='menu__icon menu__icon--delivery'></div>
+                    <div className="menu__list">
+                      Доставка
+                    </div>
+                  </li>
+                </NavLink>
+              )
+            }
+
+            {
+              (dataDB.settings[0].pay === '') ? null : (
+                <NavLink to={`/Pay?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
+                  <li className="menu__item">
+                    <div className='menu__icon menu__icon--pay'></div>
+                    <div className="menu__list">
+                      Оплата
+                    </div>
+                  </li>
+                </NavLink>
+              )
+            }
+
+            {
+              (dataDB.settings[0].obmin === '') ? null : (
+                <NavLink to={`/Obmin?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
                   <li className="menu__item ">
-                    <div className='menu__icon menu__icon--checkout'></div>
+                    <div className='menu__icon menu__icon--obmin'></div>
                     <div className="menu__list">
-                      Мої замовлення
+                      Обмін і повернення
                     </div>
                   </li>
                 </NavLink>
-                
-                { 
-                  (dataDB.settings[0].viberContact === '' && dataDB.settings[0].phoneOne === '' && dataDB.settings[0].instaContact === '' && dataDB.settings[0].phoneTwo === '' && dataDB.settings[0].email === '' && dataDB.settings[0].telegaContact === '' ) ? null : (
-                    <NavLink to={`/Contacts?${dataDB.listBot[0].nameShop}`} onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
-                      <li className="menu__item">
-                        <div className='menu__icon menu__icon--contacts'></div>
-                        <div className="menu__list">
-                          Контакти
-                        </div>
-                      </li>
-                    </NavLink>
-                  )
-                }
-               
-                <NavLink to={`/Help?${dataDB.listBot[0].nameShop}`} onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
+              )
+            }
+
+            {
+              (dataDB.settings[0].grafik === '') ? null : (
+                <NavLink to={`/Grafik?${dataDB.listBot[0].nameShop}`} onClick={() => scrollToTop()} className='menu__link' style={getLinkStyle}>
                   <li className="menu__item">
-                    <div className='menu__icon menu__icon--help'></div>
+                    <div className='menu__icon menu__icon--time'></div>
                     <div className="menu__list">
-                      Допомога
+                      Графік роботи
                     </div>
                   </li>
                 </NavLink>
-              
+              )
+            }
 
-                <hr className='menu__lineHorizont'></hr>
-                
-                { 
-                  (dataDB.settings[0].about === '') ? null : (
-                    <NavLink to={`/About?${dataDB.listBot[0].nameShop}`} onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
-                      <li className="menu__item">
-                        <div className='menu__icon menu__icon--about'></div>
-                        <div className="menu__list">
-                          Про нас
-                        </div>
-                      </li>
-                    </NavLink>
-                  )
-                }
-                
-                { 
-                  (dataDB.settings[0].garant === '') ? null : (
-                    <NavLink to={`/Garant?${dataDB.listBot[0].nameShop}`} onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
-                      <li className="menu__item">
-                        <div className='menu__icon menu__icon--garant'></div>
-                        <div className="menu__list">
-                          Гарантійні умови
-                        </div>
-                      </li>
-                    </NavLink>
-                  )
-                }
-
-                { 
-                  (dataDB.settings[0].delivery === '') ? null : (
-                    <NavLink to={`/Delivery?${dataDB.listBot[0].nameShop}`}onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
-                      <li className="menu__item">
-                        <div className='menu__icon menu__icon--delivery'></div>
-                        <div  className="menu__list">
-                          Доставка
-                        </div>
-                      </li>
-                    </NavLink>
-                  )
-                }
-               
-                { 
-                  (dataDB.settings[0].pay === '') ? null : (
-                    <NavLink to={`/Pay?${dataDB.listBot[0].nameShop}`} onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
-                      <li className="menu__item">
-                        <div className='menu__icon menu__icon--pay'></div>
-                        <div className="menu__list">
-                          Оплата
-                        </div>
-                      </li>
-                    </NavLink>
-                  )
-                }
-                
-                { 
-                  (dataDB.settings[0].obmin === '') ? null : (
-                    <NavLink to={`/Obmin?${dataDB.listBot[0].nameShop}`}onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
-                      <li className="menu__item ">
-                        <div className='menu__icon menu__icon--obmin'></div>
-                        <div className="menu__list">
-                          Обмін і повернення
-                        </div>
-                      </li>
-                    </NavLink>
-                  )
-                }
-
-                { 
-                  (dataDB.settings[0].grafik === '') ? null : (
-                    <NavLink to={`/Grafik?${dataDB.listBot[0].nameShop}`}onClick={() =>scrollToTop()} className='menu__link' style={getLinkStyle}>
-                      <li className="menu__item">
-                        <div className='menu__icon menu__icon--time'></div>
-                        <div className="menu__list">
-                          Графік роботи
-                        </div>
-                      </li>
-                    </NavLink>
-                  )
-                }
-                
-              </ul>
-          </div>       
-        </aside>
-      </>
+          </ul>
+        </div>
+      </aside>
+    </>
     }
   </>
 }
